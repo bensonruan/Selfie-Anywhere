5fa8d679b4e21c5b9b9a15736e74c8ebd61d7897
Benson Ruan <benson_ruan@hotmail.com>
2020-05-22 23:25:32 +1000

    upgrade to body-pix2.0
    upgrade to body-pix2.0 model

--- js/selfie-anywhere.js
+++ js/selfie-anywhere.js
@@ -7,135 +7,134 @@ const segmentationThreshold = 0.5;
 const backgrounds = ["greatwall", "pyramid", "Colosseum", "monchu", "ayers-rock","taj", "easter-island", "moon"];
 const backgroundImagesPath =  'images/';
 const snapSound = new Audio('audio/snap.wav');
+//const backgroundImagesPath =  '/wp-content/uploads/2019/10/';
+//const snapSound = new Audio('/wp-content/uploads/2019/10/snap.wav');
 
 const contextPerson = canvasPerson.getContext("2d");
 let net;
 let cameraFrame;
 let currentBGIndex = 0;
+let screenMode;
 
 $("#webcam-switch").change(function () {
     if(this.checked){
         $('.md-modal').addClass('md-show');
-        webcam.start()
+        webcam.start(false)
             .then(result =>{
+               $('.flash').hide();
                cameraStarted();
                console.log("webcam started");
-               startDetectBody();
+               contextPerson.clearRect(0,0,canvasPerson.width,canvasPerson.height);
+               screenMode = window.innerWidth > window.innerHeight? 'l' : 'p';
+               
+               cameraFrame = startDetectBody();
             })
             .catch(err => {
                 displayError();
             });
     }
     else {        
-        cameraStopped();
+        cameraStopped(false, "selfie-anywhere-app");
         webcam.stop();
         cancelAnimationFrame(cameraFrame);
+        contextPerson.clearRect(0,0,canvasPerson.width,canvasPerson.height);
         console.log("webcam stopped");
     }        
 });
 
-function displayError(err = ''){
-    if(err!=''){
-        $("#errorMsg").html(err);
+$("#webcam").bind("loadedmetadata", function () {
+    screenModeChange();
+    if(net != null){
+        cameraFrame = detectBody();
     }
-    $("#errorMsg").removeClass("d-none");
-}
-
-function cameraStarted(){
-    $("#errorMsg").addClass("d-none");
-    $('.flash').hide();
-    $("#webcam-caption").html("on");
-    $("#webcam-control").removeClass("webcam-off");
-    $("#webcam-control").addClass("webcam-on");
-    $("#canvasPerson").addClass("d-none");
-    $(".webcam-container").removeClass("d-none");
-    $(".spinner-border").removeClass('d-none');
-    $("#wpfront-scroll-top-container").addClass("d-none");
-    window.scrollTo(0, 0); 
-    $('body').css('overflow-y','hidden');
-}
-
-function cameraStopped(){
-    $("#errorMsg").addClass("d-none");
-    $("#wpfront-scroll-top-container").removeClass("d-none");
-    $("#webcam-control").removeClass("webcam-on");
-    $("#webcam-control").addClass("webcam-off");
-    $(".webcam-container").addClass("d-none");
-    $("#webcam-caption").html("Click to Start Camera");
-    $('.md-modal').removeClass('md-show');
-}
-
-window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        window.oRequestAnimationFrame      ||
-        window.msRequestAnimationFrame     ||
-        function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-        };
-})();
-
-window.cancelAnimationFrame = (function(){
-    return  window.cancelAnimationFrame || window.mozCancelAnimationFrame;
-})();
-
+});
 
 function startDetectBody() {
-    bodyPix.load(multiplier)
-    .catch(error => {
-        console.log(error);
-    })
-    .then(objNet => {
-        $(".spinner-border").addClass('d-none');
-        net = objNet;
-        cameraFrame = detectBody();
-    });
+    webcam.stream()
+        .then(result => {
+            if(net == null){
+                $(".spinner-border").removeClass('d-none');
+                bodyPix.load({
+                    architecture: 'MobileNetV1',
+                    outputStride: outputStride,
+                    multiplier: multiplier,
+                    quantBytes: 2
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+                .then(objNet => {
+                    $(".spinner-border").addClass('d-none');
+                    net = objNet;
+                    $("#canvasPerson").removeClass("d-none");
+                    cameraFrame = detectBody();
+                });
+            }else{
+                $("#canvasPerson").removeClass("d-none");
+            }
+        })
+        .catch(err => {
+            displayError();
+        });
 }
 
 function detectBody(){
-    net.estimatePersonSegmentation(webcamElement, outputStride, segmentationThreshold)
+    net.segmentPerson(webcamElement,  {
+        flipHorizontal: false,
+        internalResolution: 'medium',
+        segmentationThreshold: segmentationThreshold
+      })
     .catch(error => {
         console.log(error);
     })
     .then(personSegmentation => {
-        drawBody(personSegmentation);
-        $("#canvasPerson").removeClass("d-none");
+        if(personSegmentation!=null){
+            drawBody(personSegmentation);
+        }
     });
     cameraFrame = requestAnimFrame(detectBody);
 }
 
 function drawBody(personSegmentation)
 {
-    var canvas = document.createElement('canvas');
-    canvas.width = webcamElement.width;
-    canvas.height = webcamElement.height;
-    var context = canvas.getContext('2d');
-    context.drawImage(webcamElement, 0, 0);
-    var imageData = context.getImageData(0,0, webcamElement.width, webcamElement.height);
-    var pixel = imageData.data;
-    for (var p = 0; p<pixel.length; p+=4)
-    {
-      if (personSegmentation.data[p/4] == 0) {
-          pixel[p+3] = 0;
-      }
-    }
-    context.imageSmoothingEnabled = true;
-    context.putImageData(imageData,0,0);
-
-    var imageObject=new Image();
-    imageObject.onload=function(){        
-        canvasPerson.width = webcamElement.scrollWidth;
-        canvasPerson.height = webcamElement.scrollHeight;
-        contextPerson.clearRect(0,0,canvasPerson.width,canvasPerson.height);
-        if(webcam.facingMode == 'user'){
-            contextPerson.translate(canvasPerson.width, 0);
-            contextPerson.scale(-1, 1);
+    if(screenMode == 'l'){
+        var canvas = document.createElement('canvas');
+        canvas.width = webcamElement.width;
+        canvas.height = webcamElement.height;
+        var context = canvas.getContext('2d');
+        context.drawImage(webcamElement, 0, 0);
+        var imageData = context.getImageData(0,0, webcamElement.width, webcamElement.height);
+        
+        var pixel = imageData.data;
+        for (var p = 0; p<pixel.length; p+=4)
+        {
+          if (personSegmentation.data[p/4] == 0) {
+              pixel[p+3] = 0;
+          }
+        }
+        context.imageSmoothingEnabled = true;
+        context.putImageData(imageData,0,0);
+    
+        var imageObject=new Image();
+        imageObject.onload=function(){        
+            contextPerson.clearRect(0,0,canvasPerson.width,canvasPerson.height);
+            contextPerson.imageSmoothingEnabled = true;
+            contextPerson.drawImage(imageObject, 0, 0, canvasPerson.width, canvasPerson.height);
+        }
+        imageObject.src=canvas.toDataURL();
+    }else {
+        contextPerson.drawImage(webcamElement, 0, 0, webcamElement.width, webcamElement.height);
+        var imageData = contextPerson.getImageData(0,0, webcamElement.width, webcamElement.height);
+        var pixel = imageData.data;
+        for (var p = 0; p<pixel.length; p+=4)
+        {
+          if (personSegmentation.data[p/4] == 0) {
+              pixel[p+3] = 0;
+          }
         }
         contextPerson.imageSmoothingEnabled = true;
-        contextPerson.drawImage(imageObject, 0, 0, canvasPerson.width, canvasPerson.height);
+        contextPerson.putImageData(imageData,0,0);
     }
-    imageObject.src=canvas.toDataURL();
 }
 
 $("#arrowLeft").click(function () {
@@ -169,53 +168,17 @@ $("#take-photo").click(function () {
         canvas.style.left = "0px";
         appendElement.appendChild(canvas);
         document.querySelector('#download-photo').href = canvas.toDataURL('image/png');
+        webcam.stop();
+        cancelAnimationFrame(cameraFrame);
         afterTakePhoto();
     });
 });
-
-function beforeTakePhoto(){
-    snapSound.play();
-    $('.flash')
-        .show() 
-        .animate({opacity: 0.5}, 1000) 
-        .fadeOut(1000)
-        .css({'opacity': 1});
-    window.scrollTo(0, 0); 
-    $('#webcam-control').addClass('d-none');
-    $('#arrowLeft').addClass('d-none');
-    $('#arrowRight').addClass('d-none');
-    $('#cameraControls').addClass('d-none');
-}
-
-function afterTakePhoto(){
-    webcam.stop();
-    cancelAnimationFrame(cameraFrame);
-    $('#selfie-container').addClass('d-none');
-    $('#take-photo').addClass('d-none');
-    $('#exit-app').removeClass('d-none');
-    $('#download-photo').removeClass('d-none');
-    $('#resume-camera').removeClass('d-none');
-    $('#cameraControls').removeClass('d-none');
-}
-
-function removeCapture(){
-    $('#captureCanvas').remove();
-    $('#selfie-container').removeClass('d-none');
-    $('#webcam-control').removeClass('d-none');
-    $('#arrowLeft').removeClass('d-none');
-    $('#arrowRight').removeClass('d-none');
-    $('#cameraControls').removeClass('d-none');
-    $('#take-photo').removeClass('d-none');
-    $('#exit-app').addClass('d-none');
-    $('#download-photo').addClass('d-none');
-    $('#resume-camera').addClass('d-none');
-}
 
 $("#resume-camera").click(function () {
     webcam.stream()
         .then(facingMode =>{
             removeCapture();
-            cameraFrame = detectBody();
+            contextPerson.clearRect(0,0,canvasPerson.width,canvasPerson.height);
         });
 });
 
@@ -223,3 +186,18 @@ $("#exit-app").click(function () {
     removeCapture();
     $("#webcam-switch").prop("checked", false).change();
 });
+
+$(window).resize(function() {
+    screenModeChange();
+});
+
+function screenModeChange(){
+    screenMode = window.innerWidth > window.innerHeight? 'l' : 'p';
+    if(screenMode == 'l'){
+        canvasPerson.style.width = '100vw';
+        canvasPerson.style.height = 'auto';
+    }else {
+        canvasPerson.style.width = window.innerWidth + 'px';
+        canvasPerson.style.height = '100vh';
+    }
+}
---
+++ js/webcam-ui-lib.js
@@ -0,0 +1,92 @@
+function displayError(err = ''){
+    if(err!=''){
+        $("#errorMsg").html(err);
+    }
+    $("#errorMsg").removeClass("d-none");
+}
+
+function cameraStarted(){
+    $("#errorMsg").addClass("d-none");
+    $("#webcam-caption").html("on");
+    $("#webcam-control").removeClass("webcam-off");
+    $("#webcam-control").addClass("webcam-on");
+    $(".webcam-container").removeClass("d-none");
+    $("#wpfront-scroll-top-container").addClass("d-none");
+    window.scrollTo(0, 0); 
+    $('body').css('overflow-y','hidden');
+}
+
+function cameraStopped(doScroll = false, appName = "face-mask-app"){
+    $("#errorMsg").addClass("d-none");
+    $("#wpfront-scroll-top-container").removeClass("d-none");
+    $("#webcam-control").removeClass("webcam-on");
+    $("#webcam-control").addClass("webcam-off");
+    $(".webcam-container").addClass("d-none");
+    $("#webcam-caption").html("Click to Start Camera");
+    $('.md-modal').removeClass('md-show');
+    if(doScroll){
+        $('body').css('overflow-y','scroll');
+        $([document.documentElement, document.body]).animate({
+            scrollTop: ($("#"+appName).offset().top - 80)
+    }, 1000);    }
+}
+
+function resizeCanvas(canvasID ="canvas"){
+    if(webcamElement != null &&  webcamElement.scrollHeight>0){
+        var ratioWebCamWidth = webcamElement.scrollHeight * (webcamElement.width/webcamElement.height);
+        var canvasWidth = (ratioWebCamWidth < window.innerWidth) ? ratioWebCamWidth : window.innerWidth;
+        $("#"+canvasID).css({width: canvasWidth, height: webcamElement.scrollHeight});
+    }
+}
+
+function beforeTakePhoto(){
+    $('.flash')
+        .show() 
+        .animate({opacity: 0.3}, 500) 
+        .fadeOut(500)
+        .css({'opacity': 0.7});
+    window.scrollTo(0, 0); 
+    $('#webcam-control').addClass('d-none');
+    $('#arrowLeft').addClass('d-none');
+    $('#arrowRight').addClass('d-none');
+    $('#cameraControls').addClass('d-none');
+}
+
+function afterTakePhoto(){
+    $('#selfie-container').addClass('d-none');
+    $('#canvas').removeClass('d-none');
+    $('#take-photo').addClass('d-none');
+    $('#exit-app').removeClass('d-none');
+    $('#download-photo').removeClass('d-none');
+    $('#resume-camera').removeClass('d-none');
+    $('#cameraControls').removeClass('d-none');
+}
+
+function removeCapture(){
+    $('#captureCanvas').remove();
+    $('#selfie-container').removeClass('d-none');
+    $('#canvas').addClass('d-none');
+    $('#webcam-control').removeClass('d-none');
+    $('#cameraControls').removeClass('d-none');
+    $('#arrowLeft').removeClass('d-none');
+    $('#arrowRight').removeClass('d-none');
+    $('#take-photo').removeClass('d-none');
+    $('#exit-app').addClass('d-none');
+    $('#download-photo').addClass('d-none');
+    $('#resume-camera').addClass('d-none');
+}
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+window.cancelAnimationFrame = (function(){
+    return  window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+})();
